a2716b00b295df2bae3571a7ec97a25aa2ec9aae
Juangarck <131199084+JuangarcDev@users.noreply.github.com>
2025-02-26 15:37:49 -0500

    Se crean los modelos de dominio basicos, falta implementar nombres y descripciones

--- turnos_api/models.py
+++ turnos_api/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User, AbstractBaseUser
 
-# Create your models here.
+# --------------------------------------
+# TABLAS DE DOMINIO
+# -------------------------------------
+
+class TipoTurno(models.Model):
+    nombre = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.nombre
+    
+class EstadoTurno(models.Model):
+    nombre = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.nombre
+    
+class Proceso(models.Model):
+    nombre = models.CharField(max_length=50, unique=True)
+    
+    def __str__(self):
+        return self.nombre
+    
+class EstadoVentanilla(models.Model):
+    nombre = models.CharField(max_length=50, unique=True)
+    
+    def __str__(self):
+        return self.nombre
+    
+
+# ---------------------------------------------
+# MODELOS PRINCIPALES
+# ---------------------------------------------
+
