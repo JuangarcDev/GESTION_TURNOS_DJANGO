ab0b372c2f3523398f011c353b0ae91a410a43ba
Juangarck <131199084+JuangarcDev@users.noreply.github.com>
2025-02-26 15:49:46 -0500

    Se crean los 2 primeros modelos, diferentes a los de dominio

--- turnos_api/models.py
+++ turnos_api/models.py
@@ -34,3 +34,24 @@ class EstadoVentanilla(models.Model):
 # MODELOS PRINCIPALES
 # ---------------------------------------------
 
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=100)
+    cedula = models.CharField(max_length=20, unique=True)
+    telefono = models.CharField(max_length=10, blank=True, null=True)
+    email = models.EmailField(max_length=100, blank=True, null=True)
+    direccion = models.CharField(max_length=100, blank=True, null=True)
+    municipio = models.CharField(max_length=100)
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.nombre} - {self.cedula}"
+    
+class Turno(models.Model):
+    id_usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name="turnos")
+    turno = models.CharField(max_length=20)
+    tipo_turno = models.ForeignKey(TipoTurno, on_delete=models.PROTECT)
+    estado = models.ForeignKey(EstadoTurno, on_delete=models.PROTECT)
+    proceso = models.ForeignKey(Proceso, on_delete=models.PROTECT)
+    fecha_turno = models.DateTimeField(auto_now_add=True)
+
+    
