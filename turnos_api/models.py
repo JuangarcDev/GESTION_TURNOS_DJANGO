4806bb897ee3c738f0dd9050033a9aac8cf57777
Juangarck <131199084+JuangarcDev@users.noreply.github.com>
2025-02-26 16:09:39 -0500

    Se agregan los modelos, de las otras tablas que no son de dominio

--- turnos_api/models.py
+++ turnos_api/models.py
@@ -54,4 +54,43 @@ class Turno(models.Model):
     proceso = models.ForeignKey(Proceso, on_delete=models.PROTECT)
     fecha_turno = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"Turno {self.turno} - {self.estado.nombre}"
     
+class Funcionario(models.Model):
+    usario = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=255)
+    nombre = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=10, blank=True, null=True)
+    email = models.EmailField(max_length=100, blank=True, null=True)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_edicion = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nombre
+    
+class Atencion(models.Model):
+    id_funcionario = models.ForeignKey(Funcionario, on_delete=models.PROTECT, related_name="atenciones")
+    id_turno = models.OneToOneField(Turno, on_delete=models.CASCADE, related_name="atencion")
+    fecha_atencion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Atención de {self.id_turno.turno} por {self.id_funcionario.nombre}"
+    
+class Ventanila(models.Model):
+    nombre = models.CharField(max_length=50, unique=True)
+    estado = models.ForeignKey(EstadoVentanilla, on_delete=models.PROTECT)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_edicion = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.nombre} - {self.estado.nombre}"
+    
+class Puesto(models.Model):
+    id_funcionario = models.ForeignKey(Funcionario, on_delete=models.CASCADE, related_name="puestos")
+    id_ventanilla = models.ForeignKey(Ventanila, on_delete=models.CASCADE, related_name="puestos")
+    fecha_ingreso = models.DateTimeField(auto_now_add=True)
+    fecha_salida = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.id_funcionario.nombre} - {self.id_ventanilla.nombre}"
