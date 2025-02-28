f193514c9caf4197e48d077d2c1e0fd41e6c73bb
Juangarck <131199084+JuangarcDev@users.noreply.github.com>
2025-02-28 13:15:02 -0500

    Se crean vistas iniciales para Funcionario, Ventanilla y Turno

--- turnos_api/views.py
+++ turnos_api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Funcionario, Ventanila, Turno
+from .serializers import FuncionarioSerializer, VentanillaSerializer, TurnoSerializer
 
 # Create your views here.
+class FuncionarioViewSet(viewsets.ModelViewSet):
+    queryset = Funcionario.objects.all()
+    serializer_class = FuncionarioSerializer
+    permission_classes = [IsAuthenticated]
+
+class VentanillaViewSet(viewsets.ModelViewSet):
+    queryset = Ventanila.objects.all()
+    serializer_class = VentanillaSerializer
+    permission_classes = [IsAuthenticated]
+
+class TurnoViewSet(viewsets.ModelViewSet):
+    queryset = Turno.objects.all()
+    serializer_class = TurnoSerializer
+    permission_classes = [IsAuthenticated]
