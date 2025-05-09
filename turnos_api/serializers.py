42cde995645cb8cc6894805dfd89da5eea268e94
Juangarck <131199084+JuangarcDev@users.noreply.github.com>
2025-05-09 12:18:50 -0500

    Se ajusta serializador de turno, para que traiga en int de tiempo maximo de espera aproximado del turno, para que se pueda consumir por los diversos servicios

--- turnos_api/serializers.py
+++ turnos_api/serializers.py
@@ -20,6 +20,7 @@ class TurnoSerializer(serializers.ModelSerializer):
     cedula_usuario = serializers.CharField(source='id_usuario.cedula', read_only=True)
     tramite_color = serializers.CharField(source='tipo_tramite.color', read_only=True)
     tipo_turno_color = serializers.CharField(source='tipo_turno.color', read_only=True)
+    tiempo_estimado_maximo = serializers.SerializerMethodField()
 
     class Meta:
         model = Turno
@@ -31,6 +32,7 @@ class TurnoSerializer(serializers.ModelSerializer):
             'turno',
             'tipo_turno',
             'tipo_turno_nombre',
+            'tiempo_estimado_maximo',
             'estado',
             'tipo_tramite',
             'tipo_tramite_nombre',
@@ -39,6 +41,13 @@ class TurnoSerializer(serializers.ModelSerializer):
             'tramite_color',
             'tipo_turno_color',
         ]
+
+    def get_tiempo_estimado_maximo(self, obj):
+        if obj.tipo_turno.id == 1:  # Prioritario
+            return 15
+        elif obj.tipo_turno.id == 2:  # General
+            return obj.tipo_tramite.tiempo_espera
+        return None
 
 
 class UsuarioSerializer(serializers.ModelSerializer):
