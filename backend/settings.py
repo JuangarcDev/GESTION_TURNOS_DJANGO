bfabc70a39b1403c08300937a0328bacab9c7d83
cyara <cmyarap@gmail.com>
2025-05-23 13:05:16 -0500

    se realizan cambios en settings

--- backend/settings.py
+++ backend/settings.py
@@ -30,7 +30,7 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
-CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', '').split(',')
+CORS_ALLOWED_ORIGINS =os.getenv('CORS_ALLOWED_ORIGINS', '').split(',')
 
 
 # Application definition
